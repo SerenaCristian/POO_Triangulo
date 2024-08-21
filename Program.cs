8d562110f785170a76d364d4cb0af18d3df5abf0
Cristian Serena <devcristian02@gmail.com>
2024-08-20 23:12:12 -0300

    Atividade POO calcula area de um triangulo com class

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Triangulo;
+
+namespace Aplicacao
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            
+            string sep = new string('-', 50);
+
+            ClasseTriangulo x = new ClasseTriangulo();
+            ClasseTriangulo y = new ClasseTriangulo();
+
+            Console.WriteLine("Programa Triângulo");
+            Console.WriteLine(sep);
+            Console.WriteLine("Digite as medidas do triângulo X: ");
+            x.A = double.Parse(Console.ReadLine());
+            x.B = double.Parse(Console.ReadLine());
+            x.C = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite as medidas do triângulo Y: ");
+            y.A = double.Parse(Console.ReadLine());
+            y.B = double.Parse(Console.ReadLine());
+            y.C = double.Parse(Console.ReadLine());
+
+            double areaX = x.Area();
+            double areaY = y.Area();
+
+            Console.WriteLine($"Área do triângulo X: {areaX:F2}");
+            Console.WriteLine($"Área do triângulo Y: {areaY:F2}");
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Triângulo X é maior");
+            }
+            else
+            {
+                Console.WriteLine("Triângulo Y é maior");
+            }
+        }
+    }
+}
+
